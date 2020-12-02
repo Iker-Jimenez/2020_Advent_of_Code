f17f76a3e04a544cd25eb4d9378561f526fcaef7
Iker Jimenez <iker.jimenez@gmail.com>
2020-12-02 17:51:45 +0000

    Fixed a bug where the same expense would be used twice in the calculations if it was equal to the remainder

--- day01/puzzle1.py
+++ day01/puzzle1.py
@@ -1,13 +1,21 @@
+from collections import Counter
 expenses_file = open("expenses.txt", "r")
 
-expenses = set()
+
 # O(N)
-for expense in expenses_file:
-    expenses.add(int(expense))
+expenses = Counter([int(expense) for expense in expenses_file])
 expenses_file.close()
+
 # O(N)
 for expense in expenses:
+    found = False
     remaining = 2020 - expense
-    if remaining in expenses:
+    if remaining == expense:
+        if expenses[expense] > 1:
+            found = True
+    elif remaining in expenses:
+        found = True
+
+    if found:
         print("Found numbers %d, %d" % (expense, remaining))
         print("Total value %d" % (remaining * expense))
--- day01/puzzle2.py
+++ day01/puzzle2.py
@@ -1,9 +1,9 @@
+from collections import Counter
 expenses_file = open("expenses.txt", "r")
 
 expenses = set()
 # O(N)
-for expense in expenses_file:
-    expenses.add(int(expense))
+expenses = Counter([int(expense) for expense in expenses_file])
 expenses_file.close()
 
 # O(N^2)
@@ -12,11 +12,14 @@ for i, expense1 in enumerate(expenses):
     for j, expense2 in enumerate(expenses, i + 1):
         #print("i=%d, j=%d" % (i, j))
         remainder = 2020 - expense1 - expense2
-        if remainder in expenses:
+        if remainder == expense1 or remainder == expense2:
+            if expenses[remainder] > 1:
+                found = True
+        elif remainder in expenses:
             found = True
-            print("Found numbers %d, %d, %d" % (expense1, expense2, remainder))
-            print("Total value %d" % (expense1 * expense2 * remainder))
             break
     if found:
+        print("Found numbers %d, %d, %d" % (expense1, expense2, remainder))
+        print("Total value %d" % (expense1 * expense2 * remainder))
         break
 
